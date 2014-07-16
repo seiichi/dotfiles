fce76132f4bfa731906022c753f14ab72063260c
Seiichi SATO <seiichisato@gmail.com>
2014-07-16 21:58:49 +0900

    remove mutt

--- .mutt/apply-solarized-colors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "x$TERM" = xdumb -o "x$TERM" = xlinux ] ; then
-  colors_muttrc=mutt-colors-solarized/mutt-colors-solarized-dark-16.muttrc
-else
-  colors_muttrc=mutt-colors-solarized/mutt-colors-solarized-dark-256.muttrc
-fi
-
-echo source ~/.mutt/$colors_muttrc
